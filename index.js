11d573da69aa940f5295a79515d87a849f0ddcf8
Marcin Kowalski <98392991+cinek9@users.noreply.github.com>
2022-02-17 19:18:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ const priceList = [
 
 const orderList = [];
 
-function TotalBudget(price, amount) {
-    var totalBudget = price * amount;
-    return totalBudget;
-}
-
 function renderPriceList() {
     const priceListElement = document.getElementById('priceList');
     priceListElement.innerHTML = '';
@@ -31,81 +26,77 @@ function renderPriceList() {
 function renderOrderList() {
     const orderListElement = document.getElementById('productsList');
     orderListElement.innerHTML = '';
+    orderListElement.appendChild(productElement);
 
     for (let i = 0; i < orderList.length; i++) {
         const productElement = document.createElement('li');
         productElement.innerText = `${orderList[i].name} x ${orderList[i].amount}`;
-        orderListElement.appendChild(productElement);
-    }
-}
 
-function checkOverBudget() {
-    const budgetElement = document.getElementById('productMaxPrice');
-
-
-    budgetElement.addEventListener('change', (event) => {
-        event.preventDefault();
-        if (event.target.value.Number >= budgetElement.value) {
-            event.target.classList.add('green');
+        if (orderList[i].amount > priceList[i].price) {
+            orderListElement.addEventListener('change', (event) => {
+                event.preventDefault();
+                event.target.classList.add('green');
+            })
         }
         else {
-            event.target.classList.add('red');
-
-        };
-    });
-}
-
-function isValidData() {
-    const productNameElement = document.getElementById('productName');
-    const productAmountElement = document.getElementById('productAmount');
-    const productMaxPriceElement = document.getElementById('productMaxPrice');
-
-    if (productNameElement.value === '') {
-        console.error('product name can not be empty');
-        return false;
+            orderListElement.addEventListener('change', (event) => {
+                event.preventDefault();
+                event.target.classList.add('red');
+            })
+        }
     }
-    if (productAmountElement.value === ''
-        || Number.isNaN(Number(productAmountElement.value))) {
-        console.error('product amount can not be empty');
-        return false;
-    }
-    if (productMaxPriceElement.value === '') {
-        console.error('product budget can not be empty');
-        return false;
-    }
-    return true;
-}
 
-function init() {
-    const myBtn = document.getElementById('myBtn');
-
-    myBtn.addEventListener('click', (event) => {
-        event.preventDefault();
-
+    function isValidData() {
         const productNameElement = document.getElementById('productName');
         const productAmountElement = document.getElementById('productAmount');
         const productMaxPriceElement = document.getElementById('productMaxPrice');
 
-        if (!isValidData()) {
-            return;
+        if (productNameElement.value === '') {
+            console.error('product name can not be empty');
+            return false;
         }
-
-        const order = {
-            name: productNameElement.value,
-            amount: productAmountElement.value,
-            budget: productMaxPriceElement.value
+        if (productAmountElement.value === ''
+            || Number.isNaN(Number(productAmountElement.value))) {
+            console.error('product amount can not be empty');
+            return false;
         }
+        if (productMaxPriceElement.value === '') {
+            console.error('product budget can not be empty');
+            return false;
+        }
+        return true;
+    }
 
-        orderList.push(order);
-        renderOrderList();
+    function init() {
+        const myBtn = document.getElementById('myBtn');
 
-        productNameElement.value = '';
-        productAmountElement.value = '';
-        productMaxPriceElement.value = '';
-    });
+        myBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const productNameElement = document.getElementById('productName');
+            const productAmountElement = document.getElementById('productAmount');
+            const productMaxPriceElement = document.getElementById('productMaxPrice');
+
+            if (!isValidData()) {
+                return;
+            }
+
+            const order = {
+                name: productNameElement.value,
+                amount: productAmountElement.value,
+                budget: productMaxPriceElement.value
+            }
+
+            orderList.push(order);
+            renderOrderList();
+
+            productNameElement.value = '';
+            productAmountElement.value = '';
+            productMaxPriceElement.value = '';
+        });
+    }
+
+    init();
+    renderPriceList();
+    renderOrderList();
 }
-
-init();
-renderPriceList();
-renderOrderList();
-checkOverBudget();
